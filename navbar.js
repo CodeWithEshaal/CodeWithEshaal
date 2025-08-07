b7ad902e0a108e585d0d66ec64291625271d96df
CodeWithEshaal <codewitheshaal@gmail.com>
2025-08-07 23:37:11 +0300

    Delete navbar.js

--- navbar.js
+++
@@ -1,9 +0,0 @@
-fetch("navbar.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("navbar").innerHTML = data;
-    if (typeof updateCartCount === "function") {
-      updateCartCount();
-    }
-  })
-  .catch(error => console.error("Error loading navbar:", error));
